c30f200489b0e95c438b21fe8ea123fea9eaff1b
yaramdotnet <yaramdotnet@outlook.com>
2016-02-05 05:58:31 +0530

    After code edit reactive working

--- textcircle.js
+++ textcircle.js
@@ -1,23 +1,36 @@
+this.Documents = new Mongo.Collection("documents");
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  
+  Template.editor.helpers({
+    docid:function(){
+      //console.log(Documents.findOne());
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+      var doc = Documents.findOne();
+      if(doc)
+      {
+        return doc._id;
+      }
+      else
+      {
+        return undefined;
+      }
+      
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if(!Documents.findOne()){ // no docuemnts yet
+
+        Documents.insert({title:"my new document"});
+
+    }
+
+
   });
 }
